5359cd0da8d3490bb2e3126348003a891ffbab24
purplebus <mailtosummer@gmail.com>
2023-01-23 11:32:26 +1100

    Add Product Routers

--- src/controllers/products/productRoutes.js
+++ src/controllers/products/productRoutes.js
@@ -1,22 +1,36 @@
 const express = require("express");
 
-const { getProducts } = require("./productControllers");
+const {
+  getProducts,
+  getProductById,
+  createProduct,
+} = require("./productControllers");
 
 const productRouter = express.Router();
 
-productRouter.get("/", (req, res) => {
+productRouter.get("/", (request, response) => {
   const products = getProducts();
-  res.json(products);
+  response.json(products);
 });
 
-productRouter.get("/:productId", (req, res) => {
-  const product = getProductById[req.params.productId];
+productRouter.get("/:productId", (request, response) => {
+  const product = getProductById[request.params.productId];
   if (!product) {
-    res.status(404).json({
+    response.status(404).json({
       data: "Product Not Found",
     });
   }
-  res.json(product);
+  response.json(product);
+});
+
+productRouter.post("/", (request, response) => {
+  const product = createProduct({
+    title: request.body.title,
+    description: request.body.description,
+    price: request.body.price,
+    stock: request.body.stock,
+  });
+  response.json(product);
 });
 
 module.exports = productRouter;
